0740f2e6b338cd2d730e93a35ee327214fa8ad0c
Afvanie <95737735+Afvanie@users.noreply.github.com>
2022-10-27 12:22:18 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 function pdo_connect_mysql() {
     $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
+    $DATABASE_USER = 'admin';
+    $DATABASE_PASS = '12345678';
     $DATABASE_NAME = 'phpcrud';
     try {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
